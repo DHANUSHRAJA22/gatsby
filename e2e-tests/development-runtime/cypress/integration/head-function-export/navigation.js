9b1b70b643ea5ae443ada71abb25a0b6044a7fc7
DHANUSH RAJA <155062318+DHANUSHRAJA22@users.noreply.github.com>
2025-08-27 16:55:28 +0530

    Update navigation.js

--- e2e-tests/development-runtime/cypress/integration/head-function-export/navigation.js
+++ e2e-tests/development-runtime/cypress/integration/head-function-export/navigation.js
@@ -1,36 +1,25 @@
 import { page, data } from "../../../shared-data/head-function-export.js"
-
 // No need to test SSR navigation (anchor tags) because it's effectively covered in the html insertion tests
-
 describe(`Head function export behavior during CSR navigation (Gatsby Link)`, () => {
   it(`should remove tags not on next page`, () => {
     cy.visit(page.basic).waitForRouteChange()
-
     cy.getTestElement(`extra-meta`)
       .invoke(`attr`, `content`)
       .should(`equal`, data.static.extraMeta)
-
     cy.getTestElement(`gatsby-link`).click().waitForRouteChange()
-
     cy.get(`[data-testid="extra-meta"]`).should(`not.exist`)
   })
-
   it(`should add tags not on next page`, () => {
     cy.visit(page.basic).waitForRouteChange()
-
     cy.get(`[data-testid="extra-meta-2"]`).should(`not.exist`)
-
     cy.getTestElement(`gatsby-link`).click().waitForRouteChange()
-
     cy.getTestElement(`extra-meta-2`)
       .invoke(`attr`, `content`)
       .should(`equal`, data.queried.extraMeta2)
   })
-
   // XXX FIXME(serhalp)
-  it.skip(`should not contain tags from old tags when we navigate to page without Head export`, () => {
+  it(`should not contain tags from old tags when we navigate to page without Head export`, () => {
     cy.visit(page.basic).waitForRouteChange()
-
     cy.getTestElement(`base`)
       .invoke(`attr`, `href`)
       .should(`equal`, data.static.base)
@@ -44,11 +33,9 @@ describe(`Head function export behavior during CSR navigation (Gatsby Link)`, ()
       .invoke(`attr`, `href`)
       .should(`equal`, data.static.link)
     cy.getTestElement(`jsonLD`).should(`have.text`, data.static.jsonLD)
-
     cy.getTestElement(`navigate-to-page-without-head-export`)
       .click()
       .waitForRouteChange()
-
     cy.getTestElement(`base`).should(`not.exist`)
     cy.getTestElement(`title`).should(`not.exist`)
     cy.getTestElement(`meta`).should(`not.exist`)
@@ -57,7 +44,6 @@ describe(`Head function export behavior during CSR navigation (Gatsby Link)`, ()
     cy.getTestElement(`link`).should(`not.exist`)
     cy.getTestElement(`jsonLD`).should(`not.exist`)
   })
-
   /**
    * Technically nodes are always removed from the DOM and new ones added (in other words nodes are not reused with different data),
    * but since this is an implementation detail we'll still test the behavior we expect as if we didn't know that.
@@ -65,7 +51,6 @@ describe(`Head function export behavior during CSR navigation (Gatsby Link)`, ()
   it(`should change meta tag values`, () => {
     // Initial load
     cy.visit(page.basic).waitForRouteChange()
-
     // Validate data from initial load
     cy.getTestElement(`base`)
       .invoke(`attr`, `href`)
@@ -79,10 +64,8 @@ describe(`Head function export behavior during CSR navigation (Gatsby Link)`, ()
     cy.getTestElement(`link`)
       .invoke(`attr`, `href`)
       .should(`equal`, data.static.link)
-
     // Navigate to a different page via Gatsby Link
     cy.getTestElement(`gatsby-link`).click()
-
     // Validate data on navigated-to page
     cy.getTestElement(`base`)
       .invoke(`attr`, `href`)
@@ -96,10 +79,8 @@ describe(`Head function export behavior during CSR navigation (Gatsby Link)`, ()
     cy.getTestElement(`link`)
       .invoke(`attr`, `href`)
       .should(`equal`, data.queried.link)
-
     // Navigate back to original page via Gatsby Link
     cy.getTestElement(`gatsby-link`).click().waitForRouteChange()
-
     // Validate data is same as initial load
     cy.getTestElement(`base`)
       .invoke(`attr`, `href`)
